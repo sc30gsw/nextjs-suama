c986c91a9602a19d7c5f3a3962be7476d1f72db9
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-05-08 16:03:55 +0900

    🩹 関数宣言に修正

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
-export const useMediaQuery = (query: string) => {
+export function useMediaQuery(query: string) {
   const [value, setValue] = useState(false)
 
   useEffect(() => {
