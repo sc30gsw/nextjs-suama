9f331c4e9ce532357a96d1fab29bdc49827f7616
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-05-12 10:56:45 +0900

    🩹 Card部品の追加

--- src/app/(protected)/(reports)/daily/today/page.tsx
+++ src/app/(protected)/(reports)/daily/today/page.tsx
@@ -35,90 +35,94 @@ export default async function DailyOfTodayPage({
       <div>
         <DailyReportsSearchUserForToday />
       </div>
-      <Card className="py-0 mt-4">
-        <Suspense
-          key={JSON.stringify({ page, userNames })}
-          fallback={
-            <table className="w-full text-sm text-left font-normal">
-              <thead className="bg-muted">
-                <tr>
-                  <th className="w-30.75 p-3">日付</th>
-                  <th className="w-28.25 p-3">ユーザー名</th>
-                  <th className="w-24.25 p-3">合計時間</th>
-                  <th className="w-87 p-3">所感</th>
-                  <th className="w-32.75 p-3">リモート勤務</th>
-                  <th className="w-24.25 p-3">提出</th>
-                  <th className="w-90 p-3">操作</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Array.from({ length: 10 }, () => (
-                  <tr key={crypto.randomUUID()} className="border-b">
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-20 h-4" />
-                    </th>
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-20 h-4" />
-                    </th>
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-15 h-4" />
-                    </th>
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-82 h-4" />
-                    </th>
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-20 h-4" />
-                    </th>
-                    <th scope="row" className="p-4">
-                      <Skeleton className="w-20 h-4" />
-                    </th>
-                    <th scope="row" className="p-4 flex items-center gap-x-2">
-                      <Skeleton className="w-26 h-9" />
-                      <Skeleton className="w-19 h-9" />
-                      <Skeleton className="w-19 h-9" />
-                    </th>
+      <Card className="py-2 mt-4 max-w-full">
+        <Card.Content>
+          <Suspense
+            key={JSON.stringify({ page, userNames })}
+            fallback={
+              <table className="w-full text-sm text-left font-normal">
+                <thead className="bg-muted">
+                  <tr>
+                    <th className="w-30.75 p-3">日付</th>
+                    <th className="w-28.25 p-3">ユーザー名</th>
+                    <th className="w-24.25 p-3">合計時間</th>
+                    <th className="w-87 p-3">所感</th>
+                    <th className="w-32.75 p-3">リモート勤務</th>
+                    <th className="w-24.25 p-3">提出</th>
+                    <th className="w-90 p-3">操作</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          }
-        >
-          {reportsPromise.then((res) => (
-            <DailyReportsTableForToday reports={res} />
-          ))}
-        </Suspense>
+                </thead>
+                <tbody>
+                  {Array.from({ length: 10 }, () => (
+                    <tr key={crypto.randomUUID()} className="border-b">
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-20 h-4" />
+                      </th>
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-20 h-4" />
+                      </th>
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-15 h-4" />
+                      </th>
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-82 h-4" />
+                      </th>
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-20 h-4" />
+                      </th>
+                      <th scope="row" className="p-4">
+                        <Skeleton className="w-20 h-4" />
+                      </th>
+                      <th scope="row" className="p-4 flex items-center gap-x-2">
+                        <Skeleton className="w-26 h-9" />
+                        <Skeleton className="w-19 h-9" />
+                        <Skeleton className="w-19 h-9" />
+                      </th>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            }
+          >
+            {reportsPromise.then((res) => (
+              <DailyReportsTableForToday reports={res} />
+            ))}
+          </Suspense>
+        </Card.Content>
+        <Card.Footer>
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center gap-x-1">
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+                <Skeleton className="h-9 w-10 rounded-md" />
+              </div>
+            }
+          >
+            {reportsPromise.then((res) => {
+              const pageCount = Math.ceil(res.total / 10)
+
+              if (page > pageCount) {
+                redirect(`/daily/today?page=${pageCount}`)
+              }
+
+              return (
+                <DailyReportsTablePaginationForToday
+                  pageCount={pageCount}
+                  page={page}
+                />
+              )
+            })}
+          </Suspense>
+        </Card.Footer>
       </Card>
-      <Suspense
-        fallback={
-          <div className="flex items-center justify-center gap-x-1">
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-            <Skeleton className="h-9 w-10 rounded-md" />
-          </div>
-        }
-      >
-        {reportsPromise.then((res) => {
-          const pageCount = Math.ceil(res.total / 10)
-
-          if (page > pageCount) {
-            redirect(`/daily/today?page=${pageCount}`)
-          }
-
-          return (
-            <DailyReportsTablePaginationForToday
-              pageCount={pageCount}
-              page={page}
-            />
-          )
-        })}
-      </Suspense>
     </div>
   )
 }
