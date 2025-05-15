83f478f9a0932907b7b1af58efe4463a8bf2f466
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-05-15 11:42:18 +0900

    🚸 defaultValue追加

--- src/features/reports/daily/components/daily-search-date-range-picker.tsx
+++ src/features/reports/daily/components/daily-search-date-range-picker.tsx
@@ -1,10 +1,26 @@
 'use client'
 
+import { type CalendarDate, parseDate } from '@internationalized/date'
+import { useQueryStates } from 'nuqs'
 import { DateRangePicker } from '~/components/ui/intent-ui/date-range-picker'
+import { dailyReportForMineSearchParamsParsers } from '~/features/reports/daily/types/search-params/daily-report-for-mine-search-params'
 
 export function DailySearchDateRangePicker() {
+  const [{ startDate, endDate }] = useQueryStates(
+    dailyReportForMineSearchParamsParsers,
+  )
+
+  const defaultValue =
+    startDate && endDate
+      ? ({
+          start: parseDate(startDate.toISOString().slice(0, 10)),
+          end: parseDate(endDate.toISOString().slice(0, 10)),
+        } as const satisfies Record<string, CalendarDate>)
+      : undefined
+
   return (
     <DateRangePicker
+      defaultValue={defaultValue}
       startName="startDate"
       endName="endDate"
       className="w-full max-w-64"
