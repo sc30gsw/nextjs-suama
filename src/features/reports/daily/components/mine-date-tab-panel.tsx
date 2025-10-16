b833ce9fd7feb70a958854d35b101e1bbf861266
Shoki Yamamoto <radyama0905@gmail.com>
2025-10-16 13:08:03 +0900

    コンポーネント作成

--- src/features/reports/daily/components/mine-date-tab-panel.tsx
+++ src/features/reports/daily/components/mine-date-tab-panel.tsx
@@ -3,29 +3,27 @@ import { redirect } from 'next/navigation'
 import { Card } from '~/components/ui/intent-ui/card'
 import { RowsPerPageSelect } from '~/components/ui/pagination/rows-per-page-select'
 import { TablePagination } from '~/components/ui/pagination/table-pagination'
-import { ProjectSummaryTable } from '~/features/reports/daily/components/project-summary-table'
+import { DailyReportsTable } from '~/features/reports/daily/components/daily-reports-table'
 import { dailyReportForMineSearchParamsCache } from '~/features/reports/daily/types/search-params/daily-report-for-mine-search-params'
 import type { client } from '~/lib/rpc'
 import { paginationSearchParamsCache } from '~/types/search-params/pagination-search-params-cache'
 
-type MineProjectTabProps = {
-  summaryPromise: Promise<
-    InferResponseType<(typeof client.api.dailies.mine)['summary']['$get'], 200>
-  >
+type MineDateTabProps = {
+  reportsPromise: Promise<InferResponseType<typeof client.api.dailies.mine.$get, 200>>
+  userId: InferResponseType<typeof client.api.dailies.mine.$get, 200>['userId']
 }
 
-export async function MineProjectTabPanel({ summaryPromise }: MineProjectTabProps) {
+export async function MineDateTabPanel({ reportsPromise, userId }: MineDateTabProps) {
   const { startDate, endDate } = dailyReportForMineSearchParamsCache.all()
   const { page, rowsPerPage } = paginationSearchParamsCache.all()
 
-  const summary = await summaryPromise
+  const reports = await reportsPromise
 
-  // ページ数の計算とリダイレクト処理
-  const pageCount = Math.ceil(summary.total / rowsPerPage)
+  const pageCount = Math.ceil(reports.total / rowsPerPage)
 
   if (page > pageCount && pageCount > 0) {
     redirect(
-      `/daily/mine?tab=project&page=${pageCount}&rowsPerPage=${rowsPerPage}&startDate=${startDate}&endDate=${endDate}`,
+      `/daily/mine?tab=date&page=${pageCount}&rowsPerPage=${rowsPerPage}&startDate=${startDate}&endDate=${endDate}`,
     )
   }
 
@@ -34,13 +32,13 @@ export async function MineProjectTabPanel({ summaryPromise }: MineProjectTabProp
       <RowsPerPageSelect />
 
       <div className="flex items-end justify-between">
-        <p className="text-sm">全 {summary.total} 件のプロジェクト</p>
-        <div className="font-bold text-lg">総合計時間: {summary.grandTotalHour} 時間</div>
+        <p className="text-sm">全 {reports.total} 件の日報</p>
+        <div className="font-bold text-lg">総合計時間: {reports.grandTotalHour} 時間</div>
       </div>
 
       <Card className="max-w-full border-t-0 pt-0 ">
         <Card.Content>
-          <ProjectSummaryTable summary={summary.summary} />
+          <DailyReportsTable<'mine'> reports={reports} userId={userId} />
         </Card.Content>
 
         <Card.Footer>
