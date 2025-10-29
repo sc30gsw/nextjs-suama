7cafabb57002966be8e7c29d86eb294c81246496
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-10-29 21:01:37 +0900

    🔧 修正: CI環境に応じたデータベース接続設定を追加

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from '~/db/schema'
 import { env } from '~/env'
 
-export const db = drizzle({
-  connection: {
-    url: env.TURSO_DATABASE_URL,
-    authToken: env.TURSO_AUTH_TOKEN,
-  },
-  schema: schema,
-})
+export const db = process.env.CI
+  ? drizzle({
+      connection: {
+        url: 'file:memory:',
+      },
+      schema: schema,
+    })
+  : drizzle({
+      connection: {
+        url: env.TURSO_DATABASE_URL,
+        authToken: env.TURSO_AUTH_TOKEN,
+      },
+      schema: schema,
+    })
