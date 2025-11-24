4dcabded3d99969f25de22fd3b948b5f8989f38d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-11-25 06:47:11 +0900

    🚸 feat: Update CsvUploader and ReportContextMenu components to use Tooltip for disabled states, enhancing user experience and indicating ongoing development.

--- src/features/report-contexts/components/csv-uploader.tsx
+++ src/features/report-contexts/components/csv-uploader.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import { FileTrigger } from '~/components/ui/intent-ui/file-trigger'
+import { IconFileUpload } from '@tabler/icons-react'
+import { buttonStyles } from '~/components/ui/intent-ui/button'
+// import { FileTrigger } from '~/components/ui/intent-ui/file-trigger'
+import { Tooltip } from '~/components/ui/intent-ui/tooltip'
 
 export function CsvUploader() {
   return (
-    <FileTrigger
-      intent="primary"
-      // TODO: ここでファイルを選択したときの処理を実装する
-      // onSelect={(e) => {
-      //   const files = Array.from(e ?? [])
-      //   const filenames = files.map((file) => file.name)
-      //   setFile(filenames)
-      // }}
-    >
-      CSVアップロード
-    </FileTrigger>
+    <Tooltip delay={0}>
+      <Tooltip.Trigger className={buttonStyles({ isDisabled: true })}>
+        {/* <FileTrigger
+          intent="primary"
+          // TODO: ここでファイルを選択したときの処理を実装する
+          onSelect={(e) => {
+            const files = Array.from(e ?? [])
+            const filenames = files.map((file) => file.name)
+            setFile(filenames)
+          }}
+        >
+          CSVアップロード
+        </FileTrigger> */}
+        <IconFileUpload className="size-4" />
+        CSVアップロード
+      </Tooltip.Trigger>
+      <Tooltip.Content>この機能は現在開発中です。</Tooltip.Content>
+    </Tooltip>
   )
 }
