7d779d2afcab128e2a49c15d327c79683eb68630
Shoki Yamamoto <radyama0905@gmail.com>
2025-12-04 17:38:56 +0900

    cacheComponentsのオプションをコメントアウトで追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler:true,
+  // cacheComponents: true,TODO:v16 のマイグレート時に有効化
   experimental: {
     useCache: true,
     authInterrupts: true,
