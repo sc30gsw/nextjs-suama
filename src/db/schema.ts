a12fefedec93b748137527472f7ff96f097f3bd7
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-12-09 22:28:47 +0900

    🏷️ Add user role management: Introduce `role` column in `users` table, create index on `role`, and update existing user roles based on retirement status.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,25 @@
 import { relations } from 'drizzle-orm'
 import { index, integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const users = sqliteTable('users', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  emailVerified: integer('email_verified', { mode: 'boolean' }).notNull(),
-  image: text('image'),
-  isRetired: integer('is_retired', { mode: 'boolean' }).notNull().default(false),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date()),
-})
+export const users = sqliteTable(
+  'users',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    emailVerified: integer('email_verified', { mode: 'boolean' }).notNull(),
+    image: text('image'),
+    isRetired: integer('is_retired', { mode: 'boolean' }).notNull().default(false),
+    role: text({ enum: ['admin', 'user'] })
+      .notNull()
+      .default('user'),
+    createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+    updatedAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date()),
+  },
+  (table) => [index('index_users_on_role').on(table.role)],
+)
 
 export const sessions = sqliteTable('sessions', {
   id: text('id')
